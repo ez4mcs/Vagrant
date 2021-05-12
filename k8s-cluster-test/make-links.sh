ad65f0447d87a75cae69a6df48235c3a2486c436
EZ <ezgoesit247@gmail.com>
2021-05-12 09:53:25 -0500

    added k8s-cluster-test deps

--- k8s-cluster-test/make-links.sh
+++ k8s-cluster-test/make-links.sh
@@ -8,11 +8,11 @@ ln -sfn ../k8s-cluster/empty.sh ./empty.sh
 ln -sfn ../k8s-cluster/controller.sh ./controller-test.sh
 ln -sfn ../k8s-cluster/master.sh ./master-test.sh
 ln -sfn ../k8s-cluster/node.sh ./node-test.sh
-ln -sfn ../k8s-cluster/root_key.sh ./root_key.sh
+ln -sfn ../k8s-cluster/rootkey.sh ./rootkey.sh
 ln -sfn ../k8s-cluster/simple.sh ./simple.sh
 ln -sfn ../k8s-cluster/sync.hosts.sh ./sync.hosts.sh
 ln -sfn ../ntp-install.sh ./ubuntu.k8s-test.sh
-ln -sfn ../k8s-cluster/vagrant_key.sh ./vagrant_key.sh
+ln -sfn ../k8s-cluster/vagrantkey.sh ./vagrantkey.sh
 ln -sfn ../k8s-cluster/config_ssh-access.sh ./config_ssh-access.sh
 ln -sfn ../k8s-cluster/test-connections.sh ./test-connections.sh
 ln -sfn ../k8s-cluster/ansible.setup.sh ./ansible.setup.sh
